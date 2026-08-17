66b19eebcbe9fbbe06534b5afedb97fe29eeac29
ashutosh lenka <ashutoshl2006@gmail.com>
2026-08-18 00:40:36 +0530

    Connect frontend to Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:5000/api" });
+const api = axios.create({
+  baseURL: "https://careerhub-job-portal-039x.onrender.com/api"
+});
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
